160446803b53a516565a9ffa83682ee4082afc81
Puneet <macbook@Puneets-MacBook-Pro.local>
2023-06-19 16:32:52 -0400

    Switching to manual cors

--- api/server.go
+++ api/server.go
@@ -2,14 +2,15 @@ package api
 
 import (
 	"fmt"
+	"net/http"
 
 	db "github.com/PuneetBirdi/golang-bank/db/sqlc"
 	"github.com/PuneetBirdi/golang-bank/token"
 	"github.com/PuneetBirdi/golang-bank/util"
+	//"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
-	"github.com/gin-contrib/cors"
 )
 
 type Server struct {
@@ -41,9 +42,7 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 
 func (server *Server) setupRouter() {
 	router := gin.Default()
-
-	router.Use(cors.Default())
-
+	router.Use(CORS())
 	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
 	
 	authRoutes.POST("/accounts", server.createAccount)
@@ -53,7 +52,8 @@ func (server *Server) setupRouter() {
 	authRoutes.POST("/transfers", server.createTransfer)
 
 	router.POST("/users", server.createUser)
-	router.POST("/users/login", server.loginUser)
+	router.POST("users/login", server.loginUser)
+	router.GET("", defaultResponse)
 
 	server.router = router
 }
@@ -65,4 +65,25 @@ func (server *Server) Start(address string) error {
 
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+func defaultResponse(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, "V1")
+}
+
+func CORS() gin.HandlerFunc {
+	fmt.Println("HIT CORS MIDDLEWARE")
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
